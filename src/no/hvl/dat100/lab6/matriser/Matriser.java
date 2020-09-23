535c81c85e9b4cde210bcd641a76250446525b19
591308 <69634817+591308@users.noreply.github.com>
2020-09-24 01:12:56 +0200

    ferdig tabell 6

--- src/no/hvl/dat100/lab6/matriser/Matriser.java
+++ src/no/hvl/dat100/lab6/matriser/Matriser.java
@@ -1,50 +1,89 @@
 package no.hvl.dat100.lab6.matriser;
 
 public class Matriser {
-
 	// a)
 	public static void skrivUt(int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("skrivUt ikke implementert");
+		for(int[] rad: matrise) {
+			for(int j: rad) {
+				System.out.print(j + " ");
+			}
+			System.out.println("");
+		}
 	}
-
 	// b)
 	public static String tilStreng(int[][] matrise) {
-
-		// TODO
-		throw new UnsupportedOperationException("tilStreng ikke implementert");
-		
-	}
+		String streng = ("");
+		for (int i=0; i<matrise.length; i++) {
+			for(int j=0; j<matrise[i].length; j++) {
+				streng += (matrise[i][j] + " ");
+		}
+		streng += ("\n");
+	} 
+		System.out.println(streng);	
+		return streng;
+}
 
 	// c)
 	public static int[][] skaler(int tall, int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("skaler ikke implementert");
-	
+		int[][] sum = new int[matrise.length][matrise.length];
+		for (int i=0; i<matrise.length; i++) 
+			for(int j=0; j<matrise[i].length; j++) 
+				sum [i][j] = matrise[i][j]*tall;
+		return sum;
 	}
 
 	// d)
 	public static boolean erLik(int[][] a, int[][] b) {
-
-		// TODO
-		throw new UnsupportedOperationException("erLik ikke implementert");
+		boolean flag = true;
+		int rad1 = a.length;
+		int col1 = a[0].length;
+		int rad2 = b.length;
+		int col2 = b[0].length;
+		if(rad1 !=rad2 || col1 != col2) {
+			System.out.println("Matriser er ikkje like");
+			flag = false;
+		} else {
+			for(int i = 0; i<rad1; i++) {
+				for(int j = 0; j<col1;j++) {
+					if(a[i][j] != b[i][j]) {
+						flag = false;
+						break;
+					}
+				}
+			}
+		}
+			return flag;
 	}
 	
 	// e)
 	public static int[][] speile(int[][] matrise) {
 
-		// TODO
-		throw new UnsupportedOperationException("speile ikke implementert");
-	
+		 for (int i = 0; i < matrise.length; i++)
+		    {
+		      if (matrise[i].length != matrise.length) throw new
+		        IllegalArgumentException("Tabellen er ikke kvadratisk!");
+
+		      for (int j = 0; j < i; j++)
+		      {
+		        int tab = matrise[i][j];  // bytter om a[i][j] og a[j][i]
+		        matrise[i][j] = matrise[j][i];
+		        matrise[j][i] = tab;
+		      }
+		    }
+		return matrise;
 	}
 
 	// f)
 	public static int[][] multipliser(int[][] a, int[][] b) {
-
-		// TODO
-		throw new UnsupportedOperationException("multipliser ikke implementert");
-	
+		int[][] product = new int[a.length][b.length];
+		for(int i = 0; i< 3; i++) {
+			for(int j= 0; j< 3; j++) {
+				for(int k= 0; k < 3; k++) {
+					product[i][j] += a[i][k]*b[k][j];
+				}
+			}
+		}
+		return product;
 	}
+	
 }
--- src/no/hvl/dat100/lab6/matriser/MatriserEnhetsTester.java
+++ src/no/hvl/dat100/lab6/matriser/MatriserEnhetsTester.java
@@ -4,14 +4,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+
 class MatriserEnhetsTester {
 
 	static int[][] a = { {1,2,3}, {4,5,6}, {7,8,9} };
 	static int[][] b = { {10,11,12}, {13,14,15}, {16,17,18} };
-		
+	
+	@Test
+	void testSkrivUt() {
+		Matriser.skrivUt(a);
+	}
+	
 	@Test
 	void testtilStreng() {
-		
 		assertEquals("1 2 3 \n4 5 6 \n7 8 9 \n",Matriser.tilStreng(a));
 	}
 

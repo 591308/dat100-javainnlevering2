53f4cf0297d23df452684364f2a514fbeb8f53a9
591308 <69634817+591308@users.noreply.github.com>
2020-09-23 19:25:47 +0200

    Ferdig implementer Tabeller

--- src/no/hvl/dat100/lab5/tabeller/Tabeller.java
+++ src/no/hvl/dat100/lab5/tabeller/Tabeller.java
@@ -2,63 +2,106 @@ package no.hvl.dat100.lab5.tabeller;
 
 public class Tabeller {
 
-	// a)
+	  // a)
 	public static void skrivUt(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("skrivUt ikke implementert");
-
-	}
-
-	// b)
+				System.out.println("[" + tabell.length + "]");
+	} 
+	
+	 //b)
 	public static String tilStreng(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("tilStreng ikke implementert");
+		String streng = ("[");
+		for (int i=0; i<tabell.length; i++) {
+			if(i != tabell.length-1) {
+				streng += (tabell[i] + ", ");					
+			} else {
+				streng += (tabell[i]);
+			}
+		}
+		streng += ("]");
+		System.out.println(streng);
+		return streng;
 		
-	}
-
+	} 
 	// c)
 	public static int summer(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("summer ikke implementert");
-	}
-
+		int sum = 0;
+		for (int i=0; i<tabell.length; i++)	{
+			sum += tabell[i];
+		}
+		System.out.println(sum);
+		/*
+		int sumWhile = 0;
+		int test = 0;
+		while (test < tabell.length) {
+			sumWhile += tabell[test];
+			test++;
+		}
+		System.out.println(sumWhile);
+		
+		int sumUtvidet = 0;
+		for (int t: tabell) {
+			sumUtvidet += t;	
+		}
+		System.out.println(sumUtvidet);
+		*/
+		return sum;
+}
+		
 	// d)
 	public static boolean finnesTall(int[] tabell, int tall) {
-
-		// TODO
-		throw new UnsupportedOperationException("finnesTall ikke implementert");
+	   
+		boolean finnes = false;
+		int i=0;
+		while (!finnes && i<tabell.length) {
+			if(tabell[i] == tall) {
+				finnes = true;
+			}
+				i++;
+		}
+		return finnes;
 	}
 
 	// e)
 	public static int posisjonTall(int[] tabell, int tall) {
-
-		// TODO
-		throw new UnsupportedOperationException("posisjonTall ikke implementert");
-
+		for(int i=0; i<tabell.length; i++)
+			if(tabell[i] == tall)
+				return i;
+		return -1;
 	}
 
 	// f)
 	public static int[] reverser(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("reverser ikke implementert");
-		
+		int nyTabell[] = new int[tabell.length];
+		for(int i=0, j = tabell.length-1; i < tabell.length && j>=0 ; i++, j--) {
+			nyTabell[i] = tabell[j];
+		}
+		return nyTabell;
 	}
 
 	// g)
 	public static boolean erSortert(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("erSortert ikke implementert");
+		boolean sortert = true;
+		int i=1;
+		while(sortert && i<tabell.length) {
+			if(tabell[i-1] <= tabell[i])
+				i++;
+			else
+				sortert = false;
+		}
+		return sortert;
 	}
 
 	// h)
 	public static int[] settSammen(int[] tabell1, int[] tabell2) {
-
-		// TODO
-		throw new UnsupportedOperationException("settSammen ikke implementert");
+		int nyLengde = tabell1.length + tabell2.length;
+		int [] nyTab = new int [nyLengde];
+		for(int i=0; i<tabell1.length; i++) {
+			nyTab[i]= tabell1[i];
+		}
+		for(int j=0; j<tabell2.length; j++) {
+			nyTab[tabell1.length+j]=tabell2[j];
+		}
+		return nyTab;
 	}
 }
--- src/no/hvl/dat100/lab5/tabeller/TabellerEnhetsTester.java
+++ src/no/hvl/dat100/lab5/tabeller/TabellerEnhetsTester.java
@@ -5,23 +5,22 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class TabellerEnhetsTester {
-
+	
+	
 	private static int[] tabell1 = { 1, 4, 6 };
 	private static int[] tabell2 = { 1 };
 	private static int[] tabell3 = {};
 	
 	@Test
 	void testSkrivUt() {
-		
 		Tabeller.skrivUt(tabell1);
 		Tabeller.skrivUt(tabell2);
 		Tabeller.skrivUt(tabell3);
 	}
-
+	
 	@Test
 	void testtilStreng() {
-
-		assertEquals("[1,4,6]", Tabeller.tilStreng(tabell1));
+		assertEquals("[1, 4, 6]", Tabeller.tilStreng(tabell1));
 		assertEquals("[1]", Tabeller.tilStreng(tabell2));
 		assertEquals("[]", Tabeller.tilStreng(tabell3));
 	}
@@ -32,6 +31,7 @@ class TabellerEnhetsTester {
 		assertEquals(1,Tabeller.summer(tabell2));
 		assertEquals(0,Tabeller.summer(tabell3));
 	}
+	
 
 	@Test
 	void testfinnesTall () {
@@ -86,4 +86,5 @@ class TabellerEnhetsTester {
 		assertArrayEquals(tabell1,Tabeller.settSammen(tabell3,tabell1));
 		assertArrayEquals(tabell1,Tabeller.settSammen(tabell1,tabell3));
 	}
+
 }
